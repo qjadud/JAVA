848568ef989222be0420c6063a87603108f12d1a
qjadud <101696651+qjadud@users.noreply.github.com>
2022-03-18 00:29:57 +0900

    Create 2588.java

---
+++ 2588.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		int c = b % 10;
+		int d = (b % 100) / 10;
+		int e = b / 100;
+		
+        System.out.println(a * c);
+        System.out.println(a * d);
+        System.out.println(a * e);
+        System.out.println(a * b);
+	}
+
+}
